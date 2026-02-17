43a17fd8b81a677f85603c54864b22f7f220463f
bpmryan <bpm_ryand2428@gmail.com>
2026-02-17 15:39:57 -0600

    Add beginning logic from aw.bobbyreed slides

--- code/script.js
+++ code/script.js
@@ -1,1 +1,24 @@
-alert("script loaded");
+// module pattern for gameboard (only need one)
+const gameboard = (function(){
+    const board = ['', '', '', '', '', '', '', ''];
+
+    const getBoard = () => board;
+    const setMark = (index, mark) => {
+        board[index] = mark;
+    };
+    const reset = () => board.fill('');
+
+    return{ getBoard, setMark, reset };
+})();
+
+// factory for players (need multiple)
+const createPlayer = (name, mark) => {
+    return { name, mark };
+};
+
+// module pattern for game controller 
+const GameController = (function() {
+    // game logic
+})
+
+
